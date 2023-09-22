6cc830e4719d98e79e0fd473404f2b361ef3afa8
jesus andres zini <Jesus.Zini@hstpathways.com>
2023-09-22 17:28:48 -0300

    trying SSE

--- backend/handlers/container.handler.go
+++ backend/handlers/container.handler.go
@@ -2,7 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"reflect"
+	"time"
 
 	"github.com/ddash/config"
 	"github.com/docker/docker/api/types"
@@ -20,6 +24,10 @@ type ContainerInfo struct {
 }
 
 func ListContainers(w http.ResponseWriter, r *http.Request) {  
+	w.Header().Set("Content-Type", "text/event-stream")
+    w.Header().Set("Cache-Control", "no-cache")
+    w.Header().Set("Connection", "keep-alive")
+
     containers, err := config.DockerCli.ContainerList(config.BgCtx, types.ContainerListOptions{})
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -42,4 +50,55 @@ func ListContainers(w http.ResponseWriter, r *http.Request) {
 
     w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(containerInfoList)
+}
+
+
+func ServerSentEvents(w http.ResponseWriter, r *http.Request) { 
+    w.Header().Set("Content-Type", "text/event-stream")
+    w.Header().Set("Cache-Control", "no-cache")
+    w.Header().Set("Connection", "keep-alive")
+
+    var prevContainerData []ContainerInfo
+
+    for { 
+        currentContainerData := getContainerData()
+ 
+        if !reflect.DeepEqual(currentContainerData, prevContainerData) { 
+           	data, err := json.Marshal(currentContainerData)
+            if err != nil { 
+                log.Println("Error encoding data:", err)
+                return 
+            }
+            fmt.Fprintf(w, "data: %s\n\n", data)
+            w.(http.Flusher).Flush()
+			/* json.NewEncoder(w).Encode(currentContainerData) */
+ 
+            prevContainerData = currentContainerData
+        }
+ 
+        time.Sleep(2 * time.Second)
+    }
+}
+
+func getContainerData() []ContainerInfo {
+    containers, err := config.DockerCli.ContainerList(config.BgCtx, types.ContainerListOptions{})
+    if err != nil { 
+        log.Printf("Error getting list of containers: %v", err)
+        return nil
+    }
+
+    var containerInfoList []ContainerInfo
+    for _, container := range containers {
+        containerInfo := ContainerInfo{
+            ID:      container.ID,
+            Names:   container.Names,
+            Image:   container.Image,
+            Status:  container.Status,
+            State:   container.State,
+            // Otras propiedades que desees incluir
+        }
+        containerInfoList = append(containerInfoList, containerInfo)
+    }
+
+    return containerInfoList
 }
