06a54a50d01ac5b8da6e3296c85298fbb626571d
jesusandres31 <jesusandreszini14@gmail.com>
2023-11-11 13:37:47 -0300

    update color

--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#1E63ED",
+      main: "#086DD7",
     },
     secondary: {
       main: "#0155BD",
